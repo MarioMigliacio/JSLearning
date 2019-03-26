841e4a43d9216d0749cb70978940fbade14a8587
Mario Migliacio <v-mamigl@microsoft.com>
2019-03-26 09:26:00 -0700

    Coding challenge 2 finished. Working correctly.

--- JSLearning/js/main.js
+++ JSLearning/js/main.js
@@ -1,8 +1,9 @@
-﻿// Your code here!
-
-
-// Coding tutorial challenge 1: 
+﻿/******************************
+* Coding tutorial challenge 1: 
+*/ 
 // height calculated in meters, mass in kgs
+
+/*
 var markHeight, markMass, markBMI, isMarkLarger;
 var johnHeight, johnMass, johnBMI, isJohnLarger;
 
@@ -22,3 +23,46 @@ console.log('Marks BMI: ' + markBMI);
 console.log('Johns BMI: ' + johnBMI);
 console.log('Is Mark larger than john? : ' + isMarkLarger);
 console.log('Vice versa, Is john larger than mark? : ' + isJohnLarger);
+*/
+
+
+
+/******************************
+* Coding tutorial challenge 2: 
+*/
+
+/*
+var jScore1 = 110, jScore2 = 110, jScore3 = 110;
+var mScore1 = 110, mScore2 = 110, mScore3 = 110;
+var maryScore1 = 110, maryScore2 = 110, maryScore3 = 114;
+
+var jTotal = jScore1 + jScore2 + jScore3;
+var mTotal = mScore1 + mScore2 + mScore3;
+var maryTotal = maryScore1 + maryScore2 + maryScore3;
+
+var jAvg = jTotal / 3;
+var mAvg = mTotal / 3;
+var maryAvg = maryTotal / 3;
+
+switch (true) {
+    case mAvg > jAvg && mAvg > maryAvg:
+        console.log('Marks average score is greater than Johns and Mary\'s scores: mark = ' + mAvg + ' john = ' + jAvg + ' Mary = ' + maryAvg);
+        break;
+    case jAvg > mAvg && jAvg > maryAvg:
+        console.log('Johns average score is greater than Mark and Mary\'s scores: mark = ' + mAvg + ' john = ' + jAvg + ' Mary = ' + maryAvg);
+        break;
+    case maryAvg > mAvg && maryAvg > jAvg:
+        console.log('Mary\'s average score is greater than Mark and John\'s scores: mark = ' + mAvg + ' john = ' + jAvg + ' Mary = ' + maryAvg);
+        break;
+    case mAvg === jAvg && mAvg === maryAvg:
+        console.log('All players had the same average scores! mark = ' + mAvg + ' john = ' + jAvg + ' Mary = ' + maryAvg);
+        break;
+    default: break;
+}
+*/
+
+
+
+
+
+
