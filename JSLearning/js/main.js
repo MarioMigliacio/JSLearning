5c11dc50aa10a2b5a23c80ce6ffb5c584c046655
MarioMigliacio <migliacio@hotmail.com>
2019-03-25 22:12:38 -0700

    Simple tutorial coding practice challenge 1 answers.

--- JSLearning/js/main.js
+++ JSLearning/js/main.js
@@ -1,1 +1,24 @@
 ﻿// Your code here!
+
+
+// Coding tutorial challenge 1: 
+// height calculated in meters, mass in kgs
+var markHeight, markMass, markBMI, isMarkLarger;
+var johnHeight, johnMass, johnBMI, isJohnLarger;
+
+markMass = 92;
+markHeight = 1.8;
+
+johnMass = 78;
+johnHeight = 2.3;
+
+markBMI = markMass / (markHeight * markHeight);
+johnBMI = johnMass / (johnHeight * johnHeight);
+
+isMarkLarger = markBMI > johnBMI;
+isJohnLarger = johnBMI > markBMI;
+
+console.log('Marks BMI: ' + markBMI);
+console.log('Johns BMI: ' + johnBMI);
+console.log('Is Mark larger than john? : ' + isMarkLarger);
+console.log('Vice versa, Is john larger than mark? : ' + isJohnLarger);
